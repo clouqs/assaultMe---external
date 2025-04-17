e8bab858015b9022e9560dc81d8df01b047d4db5
clouqs <zetrox.gota@gmail.com>
2025-04-17 18:40:19 +0200

    push first procid

--- assaultMe/proc.cpp
+++ assaultMe/proc.cpp
@@ -1,8 +1,23 @@
 #include "proc.h"
 
-DWORD GetProcId(const wchar_t* procName) {
+DWORD GetProcId(const wchar_t* procName) { // This function takes a process name as input and returns its process ID.
 	DWORD procId = 0;
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+		if (Process32First(hSnapshot, &procEntry)) {
+			do {
+				if (!_wcsicmp(procEntry.szExeFile, procName)) {
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnapshot, &procEntry));
+		}
+		CloseHandle(hSnapshot);
+	}
+
+
 }
 
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
